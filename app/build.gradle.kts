cd5dd68bc96bd8cd80d68f11746390bff45a0ab6
Haridian <haridian.code@gmail.com>
2025-10-09 18:19:23 +0100

    Dependency block

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,11 +12,12 @@ android {
         applicationId = "com.tech.estudiatai"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
 
     buildTypes {
         release {
@@ -25,6 +26,14 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+    }
+    android {
+        dependenciesInfo {
+            // Disables dependency metadata when building APKs.
+            includeInApk = false
+            // Disables dependency metadata when building Android App Bundles.
+            includeInBundle = false
         }
     }
     compileOptions {
