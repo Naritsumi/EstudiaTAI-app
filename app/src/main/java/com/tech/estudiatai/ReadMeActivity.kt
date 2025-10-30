8a234ea6e68a34d433faaeb2fcff3b71f31afbb9
Your Name <haridian.code@gmail.com>
2025-10-30 12:34:33 +0000

    Clear unused import

--- app/src/main/java/com/tech/estudiatai/ReadMeActivity.kt
+++ app/src/main/java/com/tech/estudiatai/ReadMeActivity.kt
@@ -32,7 +32,6 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tech.estudiatai.ui.theme.AppTheme
